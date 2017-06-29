99cfb090780e4d4f9b10707b3a368833d559ac50
Paul Wooten <pawooten@gmail.com>
2017-06-29 08:30:36 -0400

    Adding static-server.ts to initialize the express server

---
+++ static-server.ts
@@ -0,0 +1,15 @@
+import * as express from "express";
+import * as path from "path";
+
+const app = express();
+
+app.use('/',             express.static(path.join(__dirname, '..', 'client')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.html'));
+});
+
+const server = app.listen(8000, "localhost", () => {
+    const {address, port} = server.address();
+    console.log('Listening on %s %s', address, port);
+});
